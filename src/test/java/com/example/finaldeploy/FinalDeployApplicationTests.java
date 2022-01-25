e86d4f25b6b2ea26d9d7342c6a7a53e6453d202e
anuradhakumar2021 <k_anu_bpt@yahoo.co.in>
2022-01-25 16:46:34 -0500

    Add files via upload
    added spring essentials!

---
+++ src/test/java/com/example/finaldeploy/FinalDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finaldeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
